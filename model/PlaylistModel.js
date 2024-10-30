3966d262363ec0258c60edb7264c3f072e3c082b
Văn Nghĩa - Love <127735777+HVNN16@users.noreply.github.com>
2024-10-30 14:01:52 +0700

    aaa

--- model/PlaylistModel.js
+++ model/PlaylistModel.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
 const trackSchema = new mongoose.Schema({
-  title: String,
+  title: { type: String, required: true, default: "Unknown Title" }, // Đặt giá trị mặc định nếu không có
   artist: {
-    name: String,
+    name: { type: String, required: true, default: "Unknown Artist" }, // Đặt giá trị mặc định cho artist
   },
 });
 
 const playlistSchema = new mongoose.Schema({
-  playlist_id: String,
-  name: String,
-  description: String,
-  cover_url: String,
-  tracks: [trackSchema],
+  playlist_id: { type: String, required: true },
+  name: { type: String, required: true, default: "Unknown Playlist" }, // Đặt giá trị mặc định cho tên playlist
+  description: { type: String, default: "" }, // Mô tả có thể để trống
+  cover_url: { type: String, default: "" }, // URL có thể để trống nếu không có ảnh
+  tracks: { type: [trackSchema], default: [] }, // Đặt mảng mặc định rỗng nếu không có track nào
 });
 
 module.exports = mongoose.model("Playlist", playlistSchema);
